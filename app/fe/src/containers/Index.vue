4d6a3ab3b70429ec4b8d122e5c08a197989ea522
JasonFF <dfQ_Qjb@qq.com>
2018-04-12 17:29:31 +0800

    add

---
+++ app/fe/src/containers/Index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="index">
+    <header class="px-5">header</header>
+    <div class="mx-auto">
+      
+    index
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Index'
+}
+</script>
---
+++ app/router.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const { router, controller } = app;
+  router.get('/api', controller.api.index)
+  router.get('/*', controller.home.index);
+};
