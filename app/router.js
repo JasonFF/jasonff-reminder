a56b3f3f5d92635f64f24fdc968f29f2d4f150a4
JasonFF <dfQ_Qjb@qq.com>
2018-04-26 13:56:12 +0800

    add

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 'use strict';
 const httpProxy = require('http-proxy-middleware');
 const k2c = require('koa-connect');
-const proxy = k2c(httpProxy({
+const hbproxy = k2c(httpProxy({
   target: 'https://api.huobipro.com',
   changeOrigin: true,
   pathRewrite: {
@@ -12,11 +12,38 @@ const proxy = k2c(httpProxy({
     proxyRes.headers['Access-Control-Allow-Method'] = '*';
   },
 }));
+
+const zbproxy = k2c(httpProxy({
+  target: 'http://api.zb.com/data/v1',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/zbapi/*' : '/',     // rewrite path
+  },
+  onProxyRes (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    proxyRes.headers['Access-Control-Allow-Method'] = '*';
+  },
+}));
+
+const zbtradeproxy = k2c(httpProxy({
+  target: 'https://trade.zb.com/api',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/zbtradeapi/*' : '/',     // rewrite path
+  },
+  onProxyRes (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    proxyRes.headers['Access-Control-Allow-Method'] = '*';
+  },
+}));
+
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
-  router.all('/hbapi/*', proxy)
+  router.all('/hbapi/*', hbproxy)
+  router.all('/zbapi/*', zbproxy)
+  router.all('/zbtradeapi/*', zbtradeproxy)
   router.get(/^\/(?!public)/, controller.home.index);
 };
