a15dce4c8591e889c2ac846f70702f74fa6335ba
JasonFF <dfQ_Qjb@qq.com>
2018-04-26 16:05:17 +0800

    add

--- app/fe/src/router/index.js
+++ app/fe/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './Home'
+import Zb from './Zb'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/zb',
+      name: 'zb',
+      component: Zb
     }
   ]
 })
